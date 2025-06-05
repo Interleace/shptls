a7cfc2f7f58d79d119e9aa6e7ef6847fac9d0b50
Interleace <mraymer291@gmail.com>
2025-06-05 04:45:46 +0200

    Updated main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from version import get_current_version, check_for_update_and_handle
+#from version import get_current_version, check_for_update_and_handle
 import customtkinter as ctk
 from main_window import MainWindow
 from config import Config
 
 def main():
-    current_version = get_current_version()
-    print(f"Aktuelle Version: {current_version}")
-    check_for_update_and_handle(current_version)
+    #current_version = get_current_version()
+    #print(f"Aktuelle Version: {current_version}")
+    #check_for_update_and_handle(current_version)
 
     ctk.set_appearance_mode("light")
     ctk.set_default_color_theme("blue")
